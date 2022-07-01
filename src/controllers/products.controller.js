59e5bd64f0d2d52610321d14af201e8b37796d01
Mateo Peláez <mateo@innventto.com>
2022-06-30 23:16:28 -0500

    feat(product): Create products controller

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,70 @@
+const ProductsService = require('../services/product.service')
+const service = new ProductsService();
+
+const index = (req, res, next) => {
+  service.find()
+    .then((products) => {
+      res.status(200).json(products);
+    })
+    .catch( error => {
+      next(error)
+    } )
+}
+
+const show = (req, res, next) => {
+  const { id } = req.params;
+  const { msg } = req.query;
+  console.log(msg)
+  service.findOne(id)
+    .then(({productIndex, product}) => {
+      res.json(product);
+    })
+    .catch(error => {
+      next(error);
+    })
+}
+
+const create = (req, res, next) => {
+  const body = req.body;
+  service.create(body)
+    .then(product => {
+      res.status(201).json({
+        message: 'created',
+        data: body
+      })
+    })
+    .catch( error => {
+      next(error)
+    } )
+}
+
+const update = (req, res, next) => {
+  const { id } = req.params;
+  const body = req.body;
+  service.update(id, body)
+    .then(product => {
+      res.json({
+        message: 'updated',
+        data: product
+      })
+    })
+    .catch( error => {
+      next(error)
+    } )
+}
+
+const destroy = (req, res, next) => {
+  const { id } = req.params;
+  service.delete(id)
+    .then( product => {
+      res.json({
+        message: 'deleted',
+        data: product
+      })
+    })
+    .catch(error => {
+      next(error)
+    })
+}
+
+module.exports = { index, show, create, update, destroy  }
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,87 +1,38 @@
 const express = require('express');
 const ProductsService = require('../services/product.service')
 
+const ProductsController = require('../controllers/products.controller')
 const router = express.Router();
 const service = new ProductsService();
+
+// validations
 const validatorHandler = require('../middelwares/validator.handler')
 const {createProductSchema, updateProductSchema, getProductSchema, deleteProductSchema} = require('../schemas/product.schema')
 
 
-router.get('/', (req, res, next) => {
-  service.find()
-    .then((products) => {
-      res.status(200).json(products);
-    })
-    .catch( error => {
-      next(error)
-    } )
-});
+router.get('/', ProductsController.index);
 
 router.get('/:id',
   validatorHandler(getProductSchema, 'params') ,
-  (req, res, next) => {
-    const { id } = req.params;
-    const { msg } = req.query;
-    console.log(msg)
-    service.findOne(id)
-      .then(({productIndex, product}) => {
-        res.json(product);
-      })
-      .catch(error => {
-        next(error);
-      })
-  });
+  ProductsController.show
+);
 
 router.post('/',
   validatorHandler(createProductSchema, 'body'),
-  (req, res, next) => {
-  const body = req.body;
-  service.create(body)
-    .then(product => {
-      res.status(201).json({
-        message: 'created',
-        data: body
-      })
-    })
-    .catch( error => {
-      next(error)
-    } )
-});
+  ProductsController.create
+);
 
 
 router.patch('/:id',
   validatorHandler(getProductSchema, 'params'),
   validatorHandler(updateProductSchema, 'body'),
-  (req, res, next) => {
-  const { id } = req.params;
-  const body = req.body;
-  service.update(id, body)
-    .then(product => {
-      res.json({
-        message: 'updated',
-        data: product
-      })
-    })
-    .catch( error => {
-      next(error)
-    } )
-});
+  ProductsController.update
+);
 
 
 router.delete('/:id',
   validatorHandler(getProductSchema, 'params'),
-  (req, res, next) => {
-  const { id } = req.params;
-  service.delete(id)
-    .then( product => {
-      res.json({
-        message: 'deleted',
-        data: product
-      })
-    })
-    .catch(error => {
-      next(error)
-    })
-});
+  ProductsController.destroy
+);
 
 module.exports = router;
